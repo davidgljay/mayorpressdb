c09b6252353d4c109bbbbcddd6e7fc76c2f0a8f6
David Jay <davidgljay@gmail.com>
2015-12-22 01:50:46 -0500

    Adding LA and Chicago.

---
+++ cities/chicago.js
@@ -0,0 +1,70 @@
+var http = require('follow-redirects').http,
+Promise = require('promise'),
+cheerio = require('cheerio');
+
+var base_url = "http://www.cityofchicago.org/",
+main_url = base_url + "city/en/depts/mayor/press_room/press_releases.1.html?numPerPage=100"
+
+//Need to collect links, get each of them, and then extract. 
+//TODO: implement iteration through pages.
+var press_releases = [];
+
+http.get(main_url, function(res) {
+	var body = '';
+	res.on('data', function(chunk) {
+		body += chunk;
+	});
+	res.on('end', function() {
+		processBody(body);
+	})
+}).on('error', function(err) {
+	console.log(err);
+});
+
+var processBody = function(body) {
+	$ = cheerio.load(body);
+	var links = [],
+	promise_array = [];
+	$('.pressReleaseList .content a').each(function(i, elem) {
+		links.push(base_url + $(elem).attr('href'));
+	});
+	for (var i = 0; i < links.length; i++) {
+		promise_array.push(getPage(links[i]))
+	};
+	Promise.all(promise_array).then(function(releases) {
+		console.log("Promise array resolved");
+		console.log(releases);
+	});
+};
+
+var getPage = function(url) {
+	return new Promise(function(resolve, reject) {
+		http.get(url, function(res) {
+			var data = '';
+			res.on('data', function(chunk) {
+				data += chunk;
+			});
+			res.on('end', function() {
+				$ = cheerio.load(data, {
+		    		normalizeWhitespace: true
+				});
+				var content = $('#content-content');
+				var date = new Date(content.find('.black-medium-high-title').text());
+				var body = '';
+				content.find('p').each(function(i, elem) {
+					body += $(elem).text() + "\n";
+				})
+				console.log("Resolving promise");
+				resolve({
+					'title':content.find('h1').text(),
+					'body':body,
+					'date':date.toISOString(),
+					'img':'',
+					'url':url
+				});
+			})
+		}).on('error', function(err) {
+			reject(err);
+		})
+	});
+};
--- cities/la.js
+++ cities/la.js
@@ -1,24 +1,39 @@
 var cheerio = require('cheerio'),
 http = require('http');
 
-var url = "http://www.lamayor.org/press_release";
+var url = "http://www.lamayor.org/press_release?page=1",
+base_url = "http://www.lamayor.org";
+
+//TODO: iterate through pages.
 
 http.get(url, function(res) {
-
+	var press_releases = [];
 	//todo: consider making this a promise functionin utils for efficiency;
 	var body = '';
 	res.on('data', function(chunk) {
 		body += chunk;
 	});
 	res.on('end', function() {
-		console.log("stuff");
 		$ = cheerio.load(body, {
     		normalizeWhitespace: true
 		});
-		console.log("STuff");
-		// console.log($('.page_excerpt').contents());
+		$('#content .page_excerpt').each(function(i, elem) {
+			var img_url = $(elem).find('img').attr('src')[0] == "/" ? base_url + $(elem).find('img').attr('src') : $(elem).find('img').attr('src');
+			var date = new Date(/(·\s)(.+)/.exec($(elem).find('.byline').text())[2]);
+			console.log(date.toISOString());
+			// var date = 
+  			press_releases.push({
+  				"title": $(elem).find('h3').text(),
+  				"body":  $(elem).find('.excerpt span').text(),
+  				 "date":  date.toISOString(),
+  				"image": img_url,
+  				"url": base_url + $(elem).find('h3 a').attr('href')	
+  			});
+		});
 
 	})
 }).on('error', function(err) {
 	console.log(err);
 })
+
+//TODO:Save to central db.
