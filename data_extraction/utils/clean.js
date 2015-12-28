b34be84500f7bbbe1cfc44c6b02fb1f7e9eab40c
David Jay <davidgljay@gmail.com>
2015-12-28 17:30:39 -0500

    Updating utilities.

--- data_extraction/utils/clean.js
+++ data_extraction/utils/clean.js
@@ -1,10 +1,11 @@
 module.exports = function(items) {
+	var clean_items = [];
 	for (var i = items.length - 1; i >= 0; i--) {
-		items[i]
-		console.log(i);
-		if (items[i] != null) {
-			items[i].body = items[i].body.replace(/\s+/g,' ');
-		};
+		if (items[i]==null) {
+			continue;
+		}
+		items[i].body = items[i].body.replace(/\s+/g,' ');
+		clean_items.push(items[i]);
 	};
-	return items;
+	return clean_items;
 }
--- data_extraction/utils/logger.js
+++ data_extraction/utils/logger.js
@@ -1,23 +1,23 @@
-// var winston = require('winston');
-// require('winston-papertrail').Papertrail;
-// if (process.env.ENV=="dev" || process.env.ENV=="prod") {
-// 	var options={
-// 	        host: process.env.PAPERTRAIL_HOST,
-// 	        port: process.env.PAPERTRAIL_PORT
-// 	    };
-// 	var logger = new winston.Logger({
-// 	transports: [
-// 	    new winston.transports.Papertrail(options)
-// 	]
-// 	});
-// }
-// else if (process.env.ENV=="test") {
-// 	var logger = new winston.Logger({
-// 	transports: [
-// 	    new winston.transports.Console
-// 	]
-// 	});
-// }
+var winston = require('winston');
+require('winston-papertrail').Papertrail;
+if (process.env.ENV=="dev" || process.env.ENV=="prod") {
+	var options={
+	        host: process.env.PAPERTRAIL_HOST,
+	        port: process.env.PAPERTRAIL_PORT
+	    };
+	var logger = new winston.Logger({
+	transports: [
+	    new winston.transports.Papertrail(options)
+	]
+	});
+}
+else if (process.env.ENV=="test") {
+	var logger = new winston.Logger({
+	transports: [
+	    new winston.transports.Console
+	]
+	});
+}
 
 var logger = {};
 
