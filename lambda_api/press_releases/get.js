6585a4d4a7939d0f851b6ec8f97f2b2c4126ecfb
David Jay <davidgljay@gmail.com>
2015-12-28 15:48:52 -0500

    Adding stubs for lambda and mapping service.

---
+++ lambda_api/press_releases/get.js
@@ -0,0 +1,2 @@
+//Accept a "lastrecord" variable. Return most recent 100, or 100 after lastrecord.
+//Should also accept a search term. 
---
+++ mapping_service/map.js
@@ -0,0 +1,2 @@
+//Pulls press relases and generates maps overall and by city.
+//This will involve a large word freqency hashmap probably, possibly an array with mergesort.
