f6c82471fb0ef8625d4944b786e9d28c8895d3e9
David Jay <davidgljay@gmail.com>
2015-12-28 21:27:06 -0500

    Updating logic for sanjose and package.json.

--- data_extraction/cities/sanjose.js
+++ data_extraction/cities/sanjose.js
@@ -1,5 +1,5 @@
 
-var http = require('http'),
+var http = require('follow-redirects').http,
 fs = require('fs'),
 path = require('path'),
 firstdate = require('../utils/firstdate'),
@@ -17,6 +17,7 @@ module.exports = function() {
 
 		//Move through all of the pages with lists, creating an array of promises which return and parse PDFs.
 		var nextListPage = function(n) {
+			console.log(main_url.replace("{n}",n));
 			http.get(main_url.replace("{n}",n), function(res) {
 				var body = '';
 				if (res.statusCode==404) {
@@ -26,20 +27,27 @@ module.exports = function() {
 					body += chunk;
 				})
 				res.on('end', function() {
-					var links = getLinks(body, '#documentlist .header a');
+					var links = getLinks(body, base_url, '#documentList .headline a');
+					console.log(links);
 					if (links.length > 0) {
 						//Push a promise to an array
 						press_release_promises.push(getPDFsFromList(links, n));
-						nextListPage(n++);
+						nextListPage(n+1);
 					} else {
+						console.log("Done adding to promise_array:" + press_release_promises.length);
 						//When all promises are returned, flatten them into a 1d array and return.
 						Promise.all(press_release_promises).then(
 							function(results) {
+								console.log("All results returned:" + results.length);
 								var press_releases = [];
 								for (var i = results.length - 1; i >= 0; i--) {
-									press_releases.concat(results[i]);
+									press_releases = press_releases.concat(results[i]);
 								};
+								console.log(press_releases.length);
 								resolve(press_releases);
+							},
+							function(err) {
+								reject(err);
 							}
 						)		
 					}
@@ -47,7 +55,7 @@ module.exports = function() {
 				})
 			});
 		};
-		nextListPage(1);
+		nextListPage(0);
 	});
 }
 
@@ -55,38 +63,50 @@ var getPDFsFromList = function(links, n) {
 	var promise_array = [];
 
 	for (var i = links.length - 1; i >= 0; i--) {
-		promise_array.concat(getPDF(links[i], i, n));
+		promise_array.push(getPDF(links[i], i, n));
 	};
 	return Promise.all(promise_array).then(function(press_releases) {
 		var result_array = [];
+		console.log("Got PDFs from list: " + press_releases.length);
 		for (var i = press_releases.length - 1; i >= 0; i--) {
+			var date = firstdate(press_releases[i]);
+			if (date=='') {
+				continue;
+			}
 			result_array.push({
-				{
-					title: "Press Release- PDF",
-					date: firstdate(press_releases[i]),
+					title: "Press Release: PDF",
+					date: date.toISOString(),
 					body: press_releases[i],
 					url: links[i]
-
-				}
-			});
-		};		
+			});				
+		};
+		return result_array;		
 	});		
 };
 
-var getPDF = function(url, n, l) {
-	var file = fs.createWriteStream("file" + n + "-" + l + ".pdf");
+var getPDF = function(url, i, n) {
 	return new Promise(function(resolve, reject) {
+		var file = fs.createWriteStream("./pdfs/pressrelease" + i + "-" + n + ".pdf");
 		http.get(url, function(response) {
-		  response.pipe(file);
-		  response.on('end', function() {
-		  	var filePath = path.join(__dirname, './file' + n '-' + l + '.pdf');
-			extract(filePath, function (err, pages) {
-			  if (err) {
-			    reject("PDF Extraction: " + err);
-			  } else {
-			  	resolve(pages.join('\n'));
-			  }
-			})
-		  });		
+			console.log(response.statusCode);
+			if (response.statusCode == 404) {
+				resolve(null);
+			} else {
+				response.pipe(file);
+				response.on('end', function() {
+					var filePath = path.join(__dirname, '../pdfs/pressrelease' + i + '-' + n + '.pdf');
+					console.log("About to extract at:" + filePath);
+					extract(filePath, function (err, pages) {
+						if (err) {
+							console.log("PDF Extraction: " + err);
+							resolve(null);
+					  	} else {
+					  		console.log("PDF read successful");
+					  		resolve(pages.join('\n'));
+					  	};
+					});
+			  	});
+		  	};		
 		});
-});
+	});
+};
