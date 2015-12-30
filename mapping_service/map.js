9f9d04a41a78649b1bf2121490bbfbc4a3861bc1
David Jay <davidgljay@gmail.com>
2015-12-29 21:10:25 -0500

    Adding notes an package.json to mapping service.

--- mapping_service/map.js
+++ mapping_service/map.js
@@ -1,2 +1,8 @@
 //Pulls press relases and generates maps overall and by city.
 //This will involve a large word freqency hashmap probably, possibly an array with mergesort.
+
+var promise = require('Promise');
+
+var dynamo_get = function(lastId) {
+	//get next 100 from last id, then run callback.
+}
