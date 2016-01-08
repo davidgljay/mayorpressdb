fc61c3031c445791df87ff73a85b022f34a950f7
David Jay <davidgljay@gmail.com>
2016-01-08 01:28:17 -0500

    Starting work on mapping lambda.

--- mapping_service/map.js
+++
@@ -1,8 +0,0 @@
-//Pulls press relases and generates maps overall and by city.
-//This will involve a large word freqency hashmap probably, possibly an array with mergesort.
-
-var promise = require('Promise');
-
-var dynamo_get = function(lastId) {
-	//get next 100 from last id, then run callback.
-}
