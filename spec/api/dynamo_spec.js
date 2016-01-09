6d1af21c7bac2d96645c34e44ee42c8c4735b9cf
David Jay <davidgljay@gmail.com>
2016-01-09 13:36:00 -0500

    Adding stub dynamo tests.

---
+++ spec/api/dynamo_spec.js
@@ -0,0 +1,9 @@
+var dynamo = import('../../api/dynanmo');
+
+describe("Dynamo wrapper", function() {
+	describe("put_params", function() {
+		it ("should format cities correctly for put.", function() {
+			
+		})
+	})
+})
